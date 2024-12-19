28f7553365714613a1a332302a74d2ba24953f3b
nono951357 <berkileventemarko@gmail.com>
2024-12-19 11:12:55 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import CsapatApi from '../generated-client/src/api/CsapatApi';
+  import { onMount } from 'svelte';
+
+  let csapatok = [];
+
+  onMount(() => {
+    const apiInstance = new CsapatApi();
+    apiInstance.apiCsapatGet((error, data, response) => {
+      if (error) {
+        console.error(error);
+      } else {
+        csapatok = data;
+      }
+    });
+  });
+</script>
+
+<main>
+  <h1>Csapatok</h1>
+  <ul>
+    {#each csapatok as csapat}
+      <li>{csapat.csapatNev}</li>
+    {/each}
+  </ul>
+</main>
